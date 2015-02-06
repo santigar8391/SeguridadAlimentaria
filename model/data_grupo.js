ebdcc75c10815862ff63f0acfd21bb23ca4a6821
chago <santiagogarciavela@hotmail.com>
2015-02-06 01:04:39 -0500

    respaldo

---
+++ model/data_grupo.js
@@ -0,0 +1,42 @@
+var inspect = require('util').inspect;
+var Client = require('mariasql');
+
+var client = new Client();
+
+// genera la conexión a la base de datos "proyectoSeguridad"
+exports.connect = function() {
+    client.connect({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'H3rm3sSanch3z',
+        db: 'proyectoSeguridad'
+    });
+    client.on('connect', function() { console.log('Client connected'); }
+    ).on('error', function(err) { console.log('Client error: ' + err); }
+    ).on('close', function(hadError) { console.log('Client closed'); });
+}
+
+// obtiene todos los elementos de la tabla "grupo"
+exports.db_get_listado = function(cb) {
+    var data = [];
+    client.query("SELECT id_grupo, num_grupo, desc_grupo FROM grupo;")
+        .on('result', function(res) {
+            res.on('row', function(row) {
+                data.push(row);
+            })
+                .on('error', function(err) {
+                    console.log('Result error: ' + inspect(err));
+                })
+                .on('end', function(info) {
+                    console.log('Result finished successfully');
+                });
+        })
+        .on('end', function() {
+            cb(data);
+        });
+}
+
+// desconecta la base de datos
+exports.disconnect = function() {
+    client.end();
+}
